51e91980c23b03524d53e405eafb514a61aa29fc
Aborgen <alexanderborgen@gmail.com>
2020-11-09 16:59:33 -0700

    Add most of the remaining *_compile methods

--- JackAnalyzer/src/parser.py
+++ JackAnalyzer/src/parser.py
@@ -1,3 +1,5 @@
+import inspect
+
 class Parser():
   _export_xml : bool
   _tokens     : List[token]
@@ -19,26 +21,32 @@ class Parser():
 
 
   def _next_token(self) -> Optional[Token]:
-    token = self._token[self._tokenptr]
+    if self._tokenptr > len(self._tokens) - 1:
+      return None
+
+    token = self._tokens[self._tokenptr]
     self._tokenptr += 1
     return token
 
 
-  def _compile_symbol(self, token: Token) -> None:
-    if token.token_type != 'symbol':
-      raise ParserError(f'Token given is not a symbol! {token.value} [category {token.token_type}]', token.line, token.word)
-  def _compile_intConst(self, token: Token) -> None:
-    if token.token_type != 'intConst':
-      raise ParserError(f'Token given is not an intConst! {token.value} [category {token.token_type}]', token.line, token.word)
-  def _compile_stringConst(self, token: Token) -> None:
-    if token.token_type != 'stringConst':
-      raise ParserError(f'Token given is not a stringConst! {token.value} [category {token.token_type}]', token.line, token.word)
-  def _compile_identifier(self, token: Token) -> None:
-    if token.token_type != 'identifier':
-      raise ParserError(f'Token given is not an identifier! {token.value} [category {token.token_type}]', token.line, token.word)
+  def _compile_class(self) -> AClass:
+    a_class = AClass()
+    token = self._next_token()
+    a_class.deposit(token)                      # class
+    identifier = self._compile_identifier()
+    a_class.deposit(identifier)                 # identifier
+    token = self._next_token()
+    a_class.deposit(token)                      # {
+    variables = self._compile_class_variables_declaration()
+    a_class.deposit(variables)                  # class_variable_declaration* 
+    subroutines = self._compile_class_subroutines()
+    a_class.deposit(subroutines)                # subroutine* 
+    token = self._next_token()
+    a_class.deposit(token)                      # }
+    return a_class
 
 
-  def _compile_statements(self) -> list[Statement]:
+  def _compile_statements(self) -> StatementList:
     statements = StatementList()
     token = self._peek_token()
     while token.value != '}':
@@ -60,33 +68,26 @@ class Parser():
     return statements
 
       
-  def _compile_if_statement(self, token: Token) -> IfStatement:
-    if token.value != 'if':
-      raise ParserError()
-
+  def _compile_if_statement(self) -> ifStatement:
     statement = ifStatement()
+    token = self._next_token()
     statement.deposit(token)                    # if
     p_expression = _compile_parenthetical_expression()
     statement.deposit(p_e)                      # ( expression )
     c_b_statements = _compile_curly_bracket_statements()
     statement.deposit(c_b_statements)           # { statement* }
-
-    token = self._next_token()
-    statement.deposit(token)                    # ( expression )
     if self._peek_token().value == 'else':
       token = self._next_token()
-      statement.deposit(token)                  # else
+      statement.deposit(token)                  # (else
       c_b_statements = _compile_curly_bracket_statements()
-      statement.deposit(c_b_statements)         # { statement* }
+      statement.deposit(c_b_statements)         # { statement* })?
 
     return statement
 
 
-  def _compile_while_statement(self, token: Token) -> whileStatement:
-    if token.value != 'while':
-      raise ParserError()
-
+  def _compile_while_statement(self) -> whileStatement:
     statement = whileStatement()
+    token = self._next_token()
     statement.deposit(token)                    # while
     p_expression = _compile_parenthetical_expression()
     statement.deposit(p_e)                      # ( expression )
@@ -95,17 +96,15 @@ class Parser():
     return statement
 
 
-  def _compile_let_statement(self, token: Token) -> letStatement:
-    if token.value != 'let':
-      raise ParserError()
-
+  def _compile_let_statement(self) -> letStatement:
     statement = letStatement()
+    token = self._next_token()
     statement.deposit(token)                    # let
     token = self._next_token()
     statement.deposit(token)                    # identifier
     if self._peek_token().value == '[':
       b_expression = self._compile_bracket_expression()
-      statement.deposit(b_expression)           # [expression]
+      statement.deposit(b_expression)           # ([expression])?
 
     token = self._next_token()
     statement.deposit(token)                    # =
@@ -116,58 +115,125 @@ class Parser():
     return statement
 
 
-  def _compile_do_statement(self, token: Token) -> doStatement:
-    if token.value != 'do':
-      raise ParserError()
-
+  def _compile_do_statement(self) -> doStatement:
     statement = doStatement()
+    token = self._next_token()
     statement.deposit(token)                    # do
     subroutine = self._compile_subroutine_call()
     statement.deposit(subroutine)               # subroutine
     token = self._next_token()
     statement.deposit(token)                    # ;
+    return statement
     
 
-  def _compile_return_statement(self, token: Token) -> returnStatement:
-    if token.value != 'return':
-      raise ParserError()
-
+  def _compile_return_statement(self) -> returnStatement:
     statement = returnStatement()
+    token = self._next_token()
     statement.deposit(token)                    # return
     if self._peek_token().value != ';':
       expression = self._compile_expression()
-      statement.deposit(expression)             # expression
+      statement.deposit(expression)             # expression?
 
     token = self._next_token()
     statement.deposit(token)                    # ;
+    return statement
 
+
+
+
+  def _compile_class_variables_declaration(self) -> list[ClassVariable]:
+    return self._compile_parameter_or_variable_list(keywords=['static', 'field'], variable_class=ClassVariable)
+
+
+  def _compile_class_subroutines(self) -> list[Subroutine]:
+    subroutines = []
+    while self._peek_token() != '}':
+      subroutine = Subroutine()
+      self._compile_class_subroutine_declaration(subroutine)
+                                                # (constructor | method | fucntion) type identifier (parameter*)
+      token = self._next_token()
+      subroutine.deposit(token)                 # {
+      variables = self._compile_class_subroutine_variables_declaration()
+      subroutine.deposit(variables)             # subroutine_variable_declaration*
+      statements = self._compile_statements()
+      subroutine.deposit(statements)            # statement*
+      token = self._next_token()
+      subroutine.deposit(token)                 # }
+
+    return subroutines
+
+
+  def _compile_class_subroutine_declaration(self, subroutine: Subroutine) -> None:
+    token = self._next_token()
+    subroutine.deposit(token)                   # (constructor | method | function)
+    token = self._next_token()
+    subroutine.deposit(token)                   # type
+    identifier = self._compile_identifier()
+    subroutine.deposit(identifier)              # identifier
+    token = self._next_token()
+    subroutine.deposit(token)                   # (
+    if self._peek_token() != ')':
+      self._compile_class_subroutine_parameters(subroutine)
+                                                # parameter*
+    token = self._next_token()
+    subroutine.deposit(token)                   # )
+
+
+  def _compile_class_subroutine_parameters(self, subroutine: Subroutine) -> None:
+    while True:
+      token = self._next_token()
+      subroutine.deposit(token)                 # type
+      identifier = self._compile_identifier()
+      subroutine.deposit(identifier)            # identifier
+      if self._peek_token() == ')':
+        break
+
+      token = self._next_token()
+      subroutine.deposit(token)                 # ,
+
+
+  def _compile_class_subroutine_variables_declaration(self) -> list[SubroutineVariable]:
+    return self._compile_parameter_or_variable_list(keywords=['var'], variable_class=SubroutineVariable)
+
+
+  def _compile_parameter_or_variable_list(self, keywords: list[str], variable_class: Generic[G]) -> list[G]:
+    if not inspect.isclass(variable_class):
+      raise f'The provided class is not of type class: {variable_class}'
+
+    variables = []
+    while self._peek_token() in keywords:
+      variable = obj()
+      token = self._next_token()
+      variable.deposit(token)                   # keywords[n]
+      token = self._next_token()
+      variable.deposit(token)                   # type
+      identifier = self._compile_identifier()
+      variable.deposit(identifier)              # identifier
+      while self._peek_token.value != ';':
+        token = self._next_token()
+        variable.deposit(token)                 # (,
+        identifier = self._compile_identifier()
+        variable.deposit(identifier)            # identifier)*
+
+      token = self._next_token()
+      variable.deposit(token)                   # ;
+
+    return variables
+    
 
   # Used with if, else, and while
   def _compile_parenthetical_expression(self) -> list[str, Expression, str]:
+    if self._peek_token().value != '(':
+      raise ParserError()
+
     l = []
     token = self._next_token()
     l.append(token)                             # (
-    if self._peek_token().value != ')':
-      expression = self._compile_expression()
-      l.append(expression)                      # expression
-
+    expression = self._compile_expression()
+    l.append(expression)                        # expression
     token = self._next_token()
     l.append(token)                             # )
     return l
-
-
-  def _compile_parenthetical_parameters(self) -> list[str, ParameterList, str]:
-    l = []
-    token = self._next_token()
-    l.append(token)                             # (
-    if self._peek_token().value != ')':
-      expression = self._compile_subroutine_parameters()
-      l.append(expression)                      # parameter*
-
-    token = self._next_token()
-    l.append(token)                             # )
-    return l
-    
 
 
   def _compile_curly_bracket_statements(self) -> list[str, Expression, str]:
@@ -185,76 +251,21 @@ class Parser():
 
 
   def _compile_bracket_expression(self) -> list[str, Expression, str]:
+    if self._peek_token().value != '[':
+      raise ParserError()
+
     l = []
     token = self._next_token()
     l.append(token)                             # [
-    if self._peek_token().value != ']':
-      expression = self._compile_expression()
-      l.append(expression)                        # expression
-
+    expression = self._compile_expression()
+    l.append(expression)                        # expression
     token = self._next_token()
     l.append(token)                             # ]
     return l
 
 
-  def _compile_class(self) -> None:
-    a_class = AClass()
-    token = self._next_token()
-    a_class.deposit(token)                      # class
-    identifier = self._compile_identifier()
-    a_class.deposit(identifier)                 # className
-    token = self._next_token()
-    a_class.deposit(token)                      # {
-    fields = self._compile_class_variables()
-    a_class.deposit(fields)                     # classVarDec* 
-    subroutines = self._compile_subroutines()
-    a_class.deposit(subroutines)                # subroutineDec* 
-    token = self._next_token()
-    a_class.deposit(token)                      # }
-    return a_class
-
-
-  def _compile_class_variables(self) -> None:
-    variables = []
-
-
-  def _compile_class_variables(self) -> List[ClassVariables]:
-    outer_token = self._peek_token()
-    while token.value == 'static' or token.value == 'field':
-      variable = ClassVariableDeclaration()
-      token = self._next_token()
-      variable.deposit(token)                   # static | field
-      token = self._next_token()
-      variable.deposit(token)                   # type
-      identifier = self._compile_identifier()
-      variable.deposit(identifier)              # identifier
-      while self._peek_token.value != ';':
-        token = self._next_token()
-        variable.deposit(token)                 # ,
-        identifier = self._compile_identifier()
-        variable.deposit(identifier)            # identifier
-
-      token = self._next_token()
-      variable.deposit(token)                   # ;
-
-      outer_token = self._peek_token()
-        
-
-    return variables
-
-
-
-  def _compile_class_variable_declaration(self, token: Token) -> None:
-
-
-  def _compile_class_subroutine_declaration(self, token: Token) -> None:
-
-
-  def _compile_class_subroutine_parameters(self, token: Token) -> None:
-
-
-  def _compile_class_subroutine_body(self, token: Token) -> None:
-
-
-  def _compile_class_subroutine_variable_declaration(self, token: Token) -> None:
-
+  def _compile_identifier(self) -> Identifier:
+    identifier = Identifier()
+    token = self._next_token() 
+    identifier.deposit(token)                   # identifier
+    return identifier
