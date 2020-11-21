dfce322eb2328c87f3ffafd944547ba0bef53d7f
Aborgen <alexanderborgen@gmail.com>
2020-11-20 17:33:52 -0700

    Refactor construct_tree method so that both xml writing methods can make use of it

--- JackAnalyzer/src/codeWriter.py
+++ JackAnalyzer/src/codeWriter.py
@@ -11,30 +11,37 @@ class CodeWriter():
     out_file = out_file.parent / f'{out_file.name}.tokens.xml'
     root = ET.Element('tokens')
     for token in tokens:
-      ET.SubElement(root, token.token_type).text = token.value
+      CodeWriter.construct_tree(root, token)
 
-    tree = ET.ElementTree(root)
-    tree.write(out_file, encoding='utf-8', xml_declaration=True)
+    CodeWriter.finish_and_export_xml(root, out_file)
     print(f'Successfully tokenized: {out_file}')
 
 
   @staticmethod
   def export_grammar_objects_as_xml(obj: GrammarObject, out_file: Path) -> None:
-    def construct_tree(root: ET.Element, obj: Union[Token, GrammarObject]):
-      if type(obj) == Token:
-        node = ET.SubElement(root, obj.token_type).text = obj.value
-      elif isinstance(obj, GrammarObject):
-        node = ET.SubElement(root, obj.label) if obj.label else root
-        for child in obj.children:
-          construct_tree(node, child)
-      else:
-        raise Exception(f'CodeWriter does not recognize the type of provided object: {type(obj)}. [Token, GrammarObject] are supported')
-
-
     out_file = out_file.parent / f'{out_file.name}.grammar.xml'
     root = ET.Element(obj.label)
-    construct_tree(root, obj)
-    tree = ET.ElementTree(root)
+    for elem in obj.children:
+      CodeWriter.construct_tree(root, elem)
 
-    tree.write(out_file, encoding='utf-8', xml_declaration=True)
+    CodeWriter.finish_and_export_xml(root, out_file)
     print(f'Successfully parsed grammar: {out_file}')
+
+
+  @staticmethod
+  def construct_tree(root: ET.Element, obj: Union[Token, GrammarObject]) -> None:
+    if type(obj) == Token:
+      node = ET.SubElement(root, obj.token_type).text = obj.value
+    elif isinstance(obj, GrammarObject):
+      node = ET.SubElement(root, obj.label) if obj.label else root
+      for child in obj.children:
+        CodeWriter.construct_tree(node, child)
+    else:
+      print(obj)
+      raise Exception(f'CodeWriter does not recognize the type of provided object: {type(obj)}. [Token, GrammarObject] are supported')
+
+
+  @staticmethod
+  def finish_and_export_xml(root: ET.Element, out_file) -> None:
+    tree = ET.ElementTree(root)
+    tree.write(out_file, encoding='utf-8', xml_declaration=True)
